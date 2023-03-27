6f14f4b3879eea615e8b2ebf282b4e0d1e75d744
EugeneHao <eugenesun95@gmail.com>
2023-03-26 23:21:38 -0500

    update the plot

--- From Hao/0326.R
+++ From Hao/0326.R
@@ -1,6 +1,22 @@
 library(tidyverse)
 rawdat <- readxl::read_xlsx("rawdata/mypredata.xlsx")
 
+month_avg <- readxl::read_xlsx("../month_avg.xlsx") %>% 
+  filter(`品种代码()_OptCd` != "412")
+tmp <- strsplit(month_avg$year_month, "-")
+
+future_price <- data.frame(year = sapply(tmp, "[[", 1), 
+                           month = sapply(tmp, "[[", 2), 
+                           crop = "corn", 
+                           ex_price = month_avg$avg_value/1000)
+future_price$crop[month_avg$`品种代码()_OptCd` == "328"] <- "rice"
+future_price$crop[month_avg$`品种代码()_OptCd` == "335"] <- "soybean"
+future_price$crop[month_avg$`品种代码()_OptCd` == "365"] <- "wheat"
+
+future_price <- future_price %>% arrange(year, crop, month)
+saveRDS(future_price, "data/future_price.rds")
+
+
 # ex_pro_acre: expected profit per acre
 # ex_pro_acre3: expected profit 
 # ps_pro_acre: 
@@ -103,6 +119,11 @@ rawdat$yield_mu[rawdat$crop == "wheat" & rawdat$year == 2001] <-
 
 dat$yield_mu_lag <-  rawdat %>% filter(year < 2021) %>% "$"(yield_mu)
 
+# (5) price: price_lag 
+
+dat$price_lag <- rawdat %>% filter(year < 2021) %>% "$"(price)
+
+
 saveRDS(dat, "data/dat.rds")
 
 
@@ -110,27 +131,40 @@ saveRDS(dat, "data/dat.rds")
 
 # Try 1: profit = (ex_price_im - cost_im) * yield_mu + subsidy_mu_lag
 
+dat$impute <- rowSums(is.na(dat)) > 0
+
 g1 <- ggplot(data = dat, aes(y = (ex_price_im - cost_im) * yield_mu_lag + subsidy_mu_lag2, 
-                       x = year, group = crop, color = crop)) + 
-  geom_point(size = 2)+
-  geom_line(size = 1)+
-  xlab("Year") + ylab("Expected Profit (yuan/mu)") + # ggtitle("Expected Profits, current yield, lagged subsidy")+
-  labs(group = "crop")+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  theme(legend.title=element_blank(),legend.position="right") +
+                             x = year, group = crop)) + 
+  geom_point(aes(color = interaction(crop, impute, sep = "_"), shape = impute), size = 2.5) +
+  geom_line(aes(color = crop), size = 1) +
+  xlab("Year") + ylab("Expected Profit (yuan/mu)") +
+  labs(group = "crop") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.title = element_blank(), legend.position = "right") +
   scale_shape_discrete(name = "", labels = c("Corn","Rice","Soybean", "Wheat")) +
-  # scale_linetype_discrete(name = "", labels = c("Corn","Rice", "Soybean","Wheat")) +
-  geom_vline(aes(xintercept = 2016), color = "red", linetype = "dashed")+
-  geom_hline(aes(yintercept = 0), color = "grey")+
-  scale_x_continuous(breaks=seq(2000,2022,5))+
-  scale_y_continuous(breaks=seq(-300,1000, 200))+
-  scale_color_manual(name = "", values = c("#E69F00","#56B4E9", "#009E73","purple"), labels = c("Corn","Rice","Soybean","Wheat")) + 
+  geom_vline(aes(xintercept = 2016), color = "red", linetype = "dashed") +
+  geom_hline(aes(yintercept = 0), color = "grey") +
+  scale_x_continuous(breaks = seq(2000, 2022, 5)) +
+  scale_y_continuous(breaks = seq(-300, 1000, 200)) +
+  scale_color_manual(name = "",
+                     values = c("corn_FALSE" = "#E69F00", "corn_TRUE" = "black",
+                                "rice_FALSE" = "#56B4E9", "rice_TRUE" = "black",
+                                "soybean_FALSE" = "#009E73", "soybean_TRUE" = "black",
+                                "wheat_FALSE" = "purple", "wheat_TRUE" = "black", 
+                                "corn" = "#E69F00", 
+                                "rice" = "#56B4E9", 
+                                "soybean" = "#009E73", 
+                                "wheat" = "purple"),
+                     labels = c("Corn", "Rice", "Soybean", "Wheat"), 
+                     breaks = c("corn_FALSE", "rice_FALSE", "soybean_FALSE", "wheat_FALSE")) +
+  guides(shape = 'none', # remove the shape legend
+         color = guide_legend(override.aes = list(
+           color = c("#E69F00", "#56B4E9", "#009E73", "purple"),
+           size = 1,
+           label = c("Corn", "Rice", "Soybean", "Wheat")
+         ))) +
   theme_bw()
 
-dat_share <- dat %>% select(year, crop, share)
-dat_share <- rbind(dat_share, 
-                   dat %>% group_by(year) %>% summarise(crop = "other",
-                                     share = 1 - sum(share)))
 
 g2 <- ggplot(data = dat_share, aes(y = 100 * share, 
                              x = year, group = crop, color = crop)) + 
@@ -183,3 +217,14 @@ ggpubr::ggarrange(g1, g2, ncol = 1)
 # The price increase can be partly explained by the hog farming recovery since COVID.
 
 # 7. why corn share decreased in 2008? 
+# Both yields of corn and soybean reduced a lot in 2007.
+# For corn, the yield was 433.8 kg/mu in 2006, but it became 355 kg/mu in 2007, which reduced 18.16%. 
+# For soybean, the yield reduced from 129.5 kg/mu to 99.2 kg/mu, which reduced 23.4%. 
+# The future price of corn changed from 1.68 yuan/kg to 1.88 yuan/kg from 2007 to 2008. 
+# However, the future price of corn changed from 3.2 yuan/kg to 4.52 yuan/kg. 
+# The cost per kg for corn was similar between 2007 and 2008 (0.94 yuan/kg vs 0.95 yuan/kg). 
+# However, the cost for soybean reduced from 2.82 yuan/kg to 2.48 yuan/kg. 
+# Therefore, growing corn in 2008 can earn 0.19 yuan/kg more in expectation, 
+# but growing soybean in 2008 can earn 1.66 yuan/kg more in expectation. 
+
+
