9a59c3f1ab13c6d00d6ddba144d5fc34286ca338
EugeneHao <eugenesun95@gmail.com>
2023-05-07 10:00:57 -0400

    add results for oral

--- From Hao/functions/get_lag.R
+++ From Hao/functions/get_lag.R
@@ -7,11 +7,12 @@ get_lag <- function(data, oldname, lag, weight)
     for(j in unique(data$crop))
     {
       tmpdat <- data %>% filter(region == i, crop == j)
+      length <- nrow(tmpdat)
       oldvec <- tmpdat[,index] %>% unlist()
       tmpvec <- NULL
       for(k in 1:lag)
       {
-        tmpvec <- cbind(tmpvec, c(rep(oldvec[1], k), oldvec[-(22 - (1:k))]))
+        tmpvec <- cbind(tmpvec, c(rep(oldvec[1], k), oldvec[-(length + 1 - (1:k))]))
       }
       newvec <- c(newvec, as.vector(tmpvec %*% weight))
     }
